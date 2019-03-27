951e9c51d9d616ee0bce6198afc9725dd3b2c542
SoubhagyaMishra <soubhagyamishra8984@gmail.com>
2019-03-28 00:17:59 +0530

    first commit

---
+++ mail/contact-us.php
@@ -0,0 +1,32 @@
+<?php
+// Check for empty fields
+if (empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['mobile'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+    echo "No arguments Provided!";
+    return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$mobile = strip_tags(htmlspecialchars($_POST['mobile']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$recipients = array(
+  "planmbaconsultant@gmail.com",
+  "amrutha@planmba.in"
+);
+
+$to = implode(',', $recipients);
+$email_subject = "PlanMBA Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMobile: $mobile\n\nMessage:\n$message";
+$headers = "From: enquiry@planmba.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email";
+   
+mail($to, $email_subject, $email_body, $headers);
+return true;         
+?>
